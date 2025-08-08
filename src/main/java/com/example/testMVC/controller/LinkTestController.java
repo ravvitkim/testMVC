fa1a20867a1eae8f673d2653ac3ad84dc2f03f32
ravvitkim <mirkim6086@naver.com>
2025-08-08 17:41:51 +0900

    완료

--- src/main/java/com/example/testMVC/controller/LinkTestController.java
+++ src/main/java/com/example/testMVC/controller/LinkTestController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LinkTestController {
+    @GetMapping("/articles/main")
+    public String main() {
+        return "articles/main";
+    }
 
     @GetMapping("/articles/list")
     public String listAll(){
@@ -16,7 +20,8 @@ public class LinkTestController {
 
     @GetMapping("/articles/{id}")
     public String listOne(@PathVariable("id") int id, Model model){
-        model.addAttribute("info", id + "번 게시글 읽어오기 성공");
+        String msg = id + "번 게시글 읽어오기 성공";
+        model.addAttribute("msg", msg);
         return "/articles/list_one";
     }
 
@@ -24,7 +29,7 @@ public class LinkTestController {
     public String create(@RequestParam("name") String name,@RequestParam("weight") int weight,
                          @RequestParam("height") int height, Model model){
         model.addAttribute("info",
-                name + "의" + "키는" + height + "이고," + "몸무게는" + weight + "입니다.");
+                name + "의 키는" + height + "이고, 몸무게는" + weight + "입니다.");
         return "/articles/new";
     }
 
@@ -32,11 +37,31 @@ public class LinkTestController {
     public String updateOk(){
         return "/articles/update_ok";
     }
+
+
     @GetMapping("/articles/{id}/update")
     public String update(@PathVariable("id") int id, Model model){
-        model.addAttribute("info", id + "번 게시글 수정 폼 로딩 완료");
+        model.addAttribute("num", id + "번 게시글 수정 폼 로딩 완료");
         return "/articles/update";
     }
 
+    @GetMapping("/articles/{id}/delete")
+    public String deleteOK(@PathVariable("id") int id, Model model){
+        model.addAttribute("num1",id + "번 글 삭제 완료");
+        return "/articles/delete_ok";
+    }
+
+    @GetMapping("/articles/{id}/articleComment")
+    public String commentView(@PathVariable("id") int id, Model model){
+        model.addAttribute("num", id + "번 게시글의 모든 댓글보기 성공");
+        return "/articles/comment_view";
+    }
+
+    @GetMapping("/articles/{id}/articleComments/{article-comment-id}/delete")
+    public String delete(@PathVariable("id") int id,
+                         @PathVariable("article-comment-id") int commentId, Model model) {
+        model.addAttribute("num2", id + "번 게시글의" + commentId + "번째 답글 삭제 완료");
+        return "/articles/delete_ok";
+    }
 
 }
